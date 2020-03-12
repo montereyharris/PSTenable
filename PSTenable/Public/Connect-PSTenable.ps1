b20c7b58fa10e2e3f104edcf941840320c0e98fa
Harris <mharris@Chamberlain.com>
2020-03-12 12:57:30 -0500

    Update to the connect-pstenable to include Apikey

--- PSTenable/Public/Connect-PSTenable.ps1
+++ PSTenable/Public/Connect-PSTenable.ps1
@@ -15,6 +15,8 @@ function Connect-PSTenable {
         Tenable Server Name, tenable.domain.com/rest
     .PARAMETER Register
         If specified, this will cache the Credential, TenableServer, Token, and Web Session.
+    .PARAMETER ApiKey
+        If specfied PSCredential Object will be treated as a API Key.
     .INPUTS
         None
     .OUTPUTS
@@ -35,6 +37,10 @@ function Connect-PSTenable {
 
         [Parameter(Position = 2, mandatory = $false)]
         [switch]
+        $ApiKey,
+
+        [Parameter(Position = 3, mandatory = $false)]
+        [switch]
         $Register
     )
     begin {
@@ -43,50 +49,72 @@ function Connect-PSTenable {
 
     process {
 
-        # Credentials
-        $APICredential = @{
-            username       = $Credential.UserName
-            password       = $Credential.GetNetworkCredential().Password
-            releaseSession = "FALSE"
+        if($ApiKey){
+
+            $accesskey = $Credential.UserName
+            $secretkey = $Credential.GetNetworkCredential().Password
+
+        }Else{
+
+            # Credentials
+            $APICredential = @{
+                username       = $Credential.UserName
+                password       = $Credential.GetNetworkCredential().Password
+                releaseSession = "FALSE"
+            }
+
+            $SessionSplat = @{
+                URI             = "$TenableServer/token"
+                SessionVariable = "SCSession"
+                Method          = "Post"
+                ContentType     = "application/json"
+                Body            = (ConvertTo-Json $APICredential)
+                ErrorAction     = "Stop"
+                ErrorVariable   = "TenableTokenError"
+            }
+
+            $currentProgressPref = $ProgressPreference
+            $ProgressPreference = "SilentlyContinue"
+            $currentVersionTls = [Net.ServicePointManager]::SecurityProtocol
+            $currentSupportableTls = [Math]::Max($currentVersionTls.value__, [Net.SecurityProtocolType]::Tls.value__)
+            $availableTls = [enum]::GetValues('Net.SecurityProtocolType') | Where-Object { $_ -gt $currentSupportableTls }
+            $availableTls | ForEach-Object {
+                [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_
+            }
+
+            $Session = Invoke-RestMethod @SessionSplat
+
+            [Net.ServicePointManager]::SecurityProtocol = $currentVersionTls
+            $ProgressPreference = $currentProgressPref
         }
-
-        $SessionSplat = @{
-            URI             = "$TenableServer/token"
-            SessionVariable = "SCSession"
-            Method          = "Post"
-            ContentType     = "application/json"
-            Body            = (ConvertTo-Json $APICredential)
-            ErrorAction     = "Stop"
-            ErrorVariable   = "TenableTokenError"
-        }
-
-        $currentProgressPref = $ProgressPreference
-        $ProgressPreference = "SilentlyContinue"
-        $currentVersionTls = [Net.ServicePointManager]::SecurityProtocol
-        $currentSupportableTls = [Math]::Max($currentVersionTls.value__, [Net.SecurityProtocolType]::Tls.value__)
-        $availableTls = [enum]::GetValues('Net.SecurityProtocolType') | Where-Object { $_ -gt $currentSupportableTls }
-        $availableTls | ForEach-Object {
-            [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_
-        }
-
-        $Session = Invoke-RestMethod @SessionSplat
-
-        [Net.ServicePointManager]::SecurityProtocol = $currentVersionTls
-        $ProgressPreference = $currentProgressPref
     }
 
     end {
 
-        Set-PSFconfig -FullName "PSTenable.WebSession" -Value $SCSession
-        Set-PSFconfig -FullName "PSTenable.Token" -Value $Session.response.token
-        Set-PSFConfig -FullName "PSTenable.Server" -Value $TenableServer
-        Set-PSFconfig -FullName "PSTenable.Credential" -Value $Credential
+        if($ApiKey){
 
-        if ($Register -eq $true) {
-            Register-PSFConfig -FullName "PSTenable.WebSession"
-            Register-PSFConfig -FullName "PSTenable.Token"
-            Register-PSFConfig -FullName "PSTenable.Server"
-            Register-PSFConfig -FullName "PSTenable.Token"
+            Set-PSFconfig -FullName "PSTenable.accesskey" -Value $accesskey
+            Set-PSFconfig -FullName "PSTenable.secretkey" -Value $secretkey
+            Set-PSFConfig -FullName "PSTenable.Server" -Value $TenableServer
+
+            if ($Register -eq $true) {
+                Register-PSFConfig -FullName "PSTenable.accesskey"
+                Register-PSFConfig -FullName "PSTenable.secretkey"
+                Register-PSFConfig -FullName "PSTenable.Server"
+            }
+
+        }Else{
+            Set-PSFconfig -FullName "PSTenable.WebSession" -Value $SCSession
+            Set-PSFconfig -FullName "PSTenable.Token" -Value $Session.response.token
+            Set-PSFConfig -FullName "PSTenable.Server" -Value $TenableServer
+            Set-PSFconfig -FullName "PSTenable.Credential" -Value $Credential
+
+            if ($Register -eq $true) {
+                Register-PSFConfig -FullName "PSTenable.WebSession"
+                Register-PSFConfig -FullName "PSTenable.Token"
+                Register-PSFConfig -FullName "PSTenable.Server"
+                Register-PSFConfig -FullName "PSTenable.Token"
+            }
         }
     }
 }
